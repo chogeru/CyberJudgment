f88fc42d32b5e33c1c9ad33e9ee6dc4e47a40f5b
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2024-07-14 14:19:07 +0900

    materialの設定変更

---
+++ CyberJudgment/Assets/HayashiResources/Script/TimellineManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimellineManager: MonoBehaviour
+{
+    [SerializeField, Header("アクティブにするオブジェクト")]
+    public GameObject[] _objectsToActivate;
+    [SerializeField, Header("非アクティブにするオブジェクト")]
+    public GameObject[] _objectsToDeactivate;
+
+
+    /// <summary>
+    /// シグナルを受信したときに呼びだされるメソッド
+    /// </summary>
+    public void OnNotify()
+    {
+        ActivateObjects();
+        DeactivateObjects();
+    }
+
+    /// <summary>
+    /// 指定したオブジェクトをアクティブにするメソッド
+    /// </summary>
+    private void ActivateObjects()
+    {
+        foreach (var obj in _objectsToActivate)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// 指定したオブジェクトを非アクティブにするメソッド
+    /// </summary>
+    private void DeactivateObjects()
+    {
+        foreach (var obj in _objectsToDeactivate)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
