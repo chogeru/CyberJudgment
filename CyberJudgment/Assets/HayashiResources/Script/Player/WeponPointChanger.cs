82e7929785e28b9a05ddbf8a139643bb9e31c525
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2024-08-19 00:05:58 +0900

    uPoolsの処理追加

--- CyberJudgment/Assets/HayashiResources/Script/Player/WeponPointChanger.cs
+++ CyberJudgment/Assets/HayashiResources/Script/Player/WeponPointChanger.cs
@@ -1,6 +1,8 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using uPools;
 
 public class WeponPointChanger : MonoBehaviour
 {
@@ -16,10 +18,18 @@ public class WeponPointChanger : MonoBehaviour
     [SerializeField, Header("移動させるポイント")]
     private Transform m_MoveablePoint;
 
+    [SerializeField, Header("エフェクトのプレハブ")]
+    private GameObject m_EffectPrefab;
+
+    private AnimatorStateInfo previousState;
+
+
     private void Start()
     {
+        SharedGameObjectPool.Prewarm(m_EffectPrefab, 5);
         UpdatePointPosition();
     }
+
 
     private void Update()
     {
@@ -31,17 +41,24 @@ public class WeponPointChanger : MonoBehaviour
     /// </summary>
     private void UpdatePointPosition()
     {
-        //現在のアニメーションステートを所得
-        var currentState = m_Animator.GetCurrentAnimatorStateInfo(0);
-        //攻撃アニメーションが再生中かどうかを確認
-        if (currentState.IsName("NormalAttack") || currentState.IsName("StrongAttack"))
+        AnimatorStateInfo currentState = m_Animator.GetCurrentAnimatorStateInfo(0);
+
+        // アニメーションステートが変更された場合
+        if (currentState.fullPathHash != previousState.fullPathHash)
         {
-            MovePointToHand();
+            // 攻撃アニメーションが再生された瞬間
+            if (currentState.IsName("NormalAttack") || currentState.IsName("StrongAttack"))
+            {
+                MovePointToHand();
+            }
+            // 攻撃アニメーションが終了した瞬間
+            else if (previousState.IsName("NormalAttack") || previousState.IsName("StrongAttack"))
+            {
+                MovePointToBack();
+            }
         }
-        else
-        {
-            MovePointToBack();
-        }
+
+        previousState = currentState;
     }
 
     /// <summary>
@@ -52,6 +69,8 @@ public class WeponPointChanger : MonoBehaviour
         m_MoveablePoint.SetParent(m_HandPoint);
         m_MoveablePoint.localPosition = Vector3.zero;
         m_MoveablePoint.localRotation = Quaternion.identity;
+        // エフェクト生成
+        SpawnEffect(m_HandPoint.position);
     }
 
     /// <summary>
@@ -62,5 +81,33 @@ public class WeponPointChanger : MonoBehaviour
         m_MoveablePoint.SetParent(m_BackPoint);
         m_MoveablePoint.localPosition = Vector3.zero;
         m_MoveablePoint.localRotation = Quaternion.identity;
+        // エフェクト生成
+        SpawnEffect(m_BackPoint.position);
+    }
+
+    /// <summary>
+    /// エフェクトを指定された位置に生成する
+    /// </summary>
+    /// <param name="position">エフェクトを生成する位置</param>
+    private void SpawnEffect(Vector3 position)
+    {
+        // uPoolsを使用してエフェクトを生成
+        GameObject effect = SharedGameObjectPool.Rent(
+            m_EffectPrefab,
+            position,
+            Quaternion.identity);
+        // 一定時間後にエフェクトを返却
+        ReturnEffectAfterDelay(effect, 0.5f).Forget();
+
+    }
+    /// <summary>
+    /// エフェクトを指定された時間後に返却する
+    /// </summary>
+    /// <param name="effect">返却するエフェクト</param>
+    /// <param name="delay">返却までの遅延時間</param>
+    private async UniTaskVoid ReturnEffectAfterDelay(GameObject effect, float delay)
+    {
+        await UniTask.Delay((int)(delay * 1000));
+        SharedGameObjectPool.Return(effect);
     }
 }
